f747a02eed4f34280938442749362163a8e1c91d
Lucas Alves <lucas.rpa@outlook.com>
2022-02-21 16:31:26 -0300

    ex01

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button , Text, View, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+//const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function Principal() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Cadastro" component={Cadastro}/>
+        <Drawer.Screen name="Listar" component={Listar}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Cadastro() {
+  return (
+    <View>
+      <Text>Cadastro de Clientes</Text>
+      <Text></Text>
+      <Text>Nome:</Text>
+      <TextInput/>
+      <Text>Telefone:</Text>
+      <TextInput/>
+      <Button title="Salvar"/>
+    </View>
+  );
+}
+
+
+function App() {
+  return (
+    <View>
+      <Text>Principal</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Listar() {
+  return (
+    <View>
+      <Text>Listar Clientes</Text>
+      <Text></Text>
+      <Text>João Silva</Text>
+      <Text>(11)2342-2434</Text>
+      <Text></Text>
+      <Text>Maria Silva</Text>
+      <Text>(11)3453-4564</Text>
+      <Text></Text>
+      <Text>Alfredo José</Text>
+      <Text>(11)6453-3452</Text>
+    </View>
+  )
+}
+
+export default Principal;
